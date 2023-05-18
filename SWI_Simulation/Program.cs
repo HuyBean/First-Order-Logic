aaa275b43ab9e7ebe568cf6efdac3952cab181ee
David0403 <48942146+Noboroto@users.noreply.github.com>
2023-05-18 12:50:13 +0700

    Init new proj

---
+++ SWI_Simulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWI_Simulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
